93a448ce7e8db8bf47dc8bedc071366833dd50a6
rusgyn <rusgynmagsanay@gmail.com>
2024-10-23 14:06:35 -0400

    Modify test.

--- cypress/e2e/Navigation.cy.js
+++ cypress/e2e/Navigation.cy.js
@@ -5,7 +5,14 @@ describe('Navigation', () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.get("li").contains("Tuesday").click();
+    cy.contains("[data-testid=day]", "Tuesday") //Note: Add data-* attributes to make it easier to target elements in your component. data-testid="day" is in DayListItem component.
+      .click()
+      .should('have.class','day-list__item--selected');
+    //or can be
+    // cy.get("li")
+    // .contains("Tuesday")
+    // .click()
+    // .should('have.class', 'text--regular', "background-color", "rgb(242, 242, 242)"); //the 'text--regular' is the class where it contains "Tuesday"
   });
 
 
